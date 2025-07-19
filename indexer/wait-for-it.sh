6c819fdd9a70e914f2b11a02d8a9cdd559583313
jonsonpaskal220 <jonsonpaskal220@gmail.com>
2025-07-19 11:00:17 +0000

    fix: improve indexer service with wait-for-it script

--- indexer/wait-for-it.sh
+++ indexer/wait-for-it.sh
@@ -5,7 +5,6 @@ set -e
 
 host="$1"
 shift
-cmd="$@"
 
 until nc -z "$host" 9200; do
   >&2 echo "Elasticsearch is unavailable - sleeping"
@@ -13,4 +12,4 @@ until nc -z "$host" 9200; do
 done
 
 >&2 echo "Elasticsearch is up - executing command"
-exec $cmd
+exec "$@"
